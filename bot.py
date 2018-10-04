2e6093b7b0f9f0a55194a0d1aeaa23e82a87eb4f
AndrewLeee <andrewslee23@gmail.com>
2018-10-03 20:13:08 -0400

    added rank, made API keys hidden
    API keys were moved to config.py, and config.py was added to the .gitignore.
    
    The function !sl now commands the bot to say the rank of the current player or if they are unraked.

--- bot.py
+++ bot.py
@@ -1,16 +1,22 @@
+import config
+
 import aiohttp
 import asyncio
 import json
+import sys, traceback
 from discord.ext import commands
 from discord.ext.commands import Bot
 
 BOT_PREFIX = ("?", "!")
-TOKEN = "Mzc4NjQ3MzY2OTI1ODExNzM0.DpCLXg.IEiqHi4o4tF3wJByb2JZa1pYfD0"\
+TOKEN = config.discord_token
 
 client = Bot(command_prefix=BOT_PREFIX)
 
-@client.command(name="test")
-async def Test():
+@client.command(name="hi",
+                description="Greet the bot!",
+                brief="Hi!",
+                aliases=['hello', 'greetings', 'sup', 'yo'])
+async def greet():
     await client.say("hi!")
 
 @client.command(name='sl',
@@ -18,34 +24,52 @@ async def Test():
                 brief="Get your Summoner stats!",
                 aliases=['SummonerLookup'])
 async def summonerLookup(username):
-    url = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + username
+    byNameUrl = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + username
+    rankedUrl = "https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/"
     header = {
         "Origin": "https://developer.riotgames.com",
         "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-        "X-Riot-Token": "RGAPI-9f1eb46e-5dff-4681-b530-861a8becb8bd",
+        "X-Riot-Token": config.riot_api_key,
         "Accept-Language": "en-US,en;q=0.9",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
     }
 
 
     async with aiohttp.ClientSession() as session:
-        async with session.get(url, headers=header) as resp:
+        summonerId = 0
+        async with session.get(byNameUrl, headers=header) as resp:
             if (resp.status == 200):
                 response = await resp.text()
                 response = json.loads(response)
                 await client.say("Summoner " + username + " is level " + str(response['summonerLevel']) + ".")
+                summonerId = response['id']
             else:
-            	await client.say("Error: API call could not be made. "
-            		             "Either the given Summoner does not exist, or Riot Games's servers are offline.")
+                await client.say("Error: API call could not be made. "
+                                 "Either the given Summoner does not exist, or Riot Games's servers are offline.")
+                return
+
+        rankedUrl += str(summonerId)
+        print(rankedUrl)
+        async with session.get(rankedUrl, headers=header) as resp:
+            if (resp.status == 200):
+                response = await resp.text()
+                response = json.loads(response)
+                if (len(response) == 0):
+                    await client.say("Summoner " + username + " currently unranked.")
+                else:
+                    await client.say("Summoner " + username + " is currently ranked " + response[0]['tier'] + " " + response[0]['rank'] + ".")
+            else:
+                await client.say("fail")
+
 
 @summonerLookup.error
 async def summonerLookupHandler(error, ctx):
-	if isinstance(error, commands.MissingRequiredArgument):
-		await client.say("Did you forget the summoner's name?")
-		return
+    if isinstance(error, commands.MissingRequiredArgument):
+        await client.say("Did you forget the summoner's name?")
+        return
 
-	print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-	traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+    print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+    traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
 @client.event
 async def on_ready():
